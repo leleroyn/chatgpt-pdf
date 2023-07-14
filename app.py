2d2b16048681a1779f79f33b81c82c6e0bae54fa
Yong.Luo <leleroyn@qq.com>
2023-07-14 15:46:59 +0800

    优化ui与prompt

--- app.py
+++ app.py
@@ -23,11 +23,14 @@ def main():
     else:
         faiss_path = "db/sit"
 
-    if st.checkbox("是否更新模型"):
-        # 上传文件
-        pdf = st.file_uploader("上传PDF文件", type="pdf")
-        # 提取文本
-        if pdf is not None:
+    tab1, tab2 = st.tabs(["回答问题", "更新模型"])
+
+    # 上传文件
+    pdf = tab2.file_uploader("上传PDF文件", type="pdf")
+    # 提取文本
+    if pdf is not None:
+        with tab2.empty():
+            st.write("⏳正在更新模型...")
             text = ""
             with pdfplumber.open(pdf) as pdf_reader:
                 for page in pdf_reader.pages:
@@ -36,14 +39,17 @@ def main():
             knowledge = KnowledgeService(faiss_path, faiss_index)
             knowledge.gen(text, os.getenv("SPLITTER_CHUCK_SIZE"), os.getenv("SPLITTER_CHUCK_OVER_LAP"))
 
-            st.success("更新模型成功.")
-    else:
-        user_question = st.text_input("来向我提问吧：")
-        if user_question:
-            knowledge = KnowledgeService(faiss_path, faiss_index)
-            response, cb = knowledge.query(chatgpt_model, user_question, candidate_number)
-            st.write(response)
-            st.warning(cb)
+            st.success("✔️更新模型成功.")
+
+    user_question = st.chat_input("❓来向我提问吧：")
+    if user_question:
+        user_question = f"已知信息:{user_question}\n请基于上面的已知信息准确回答,如果不知道,直接回答”根据已有信息暂时无法回答您的问题，请联系客服.“,请用中文回答."
+        st_emt = st.empty()
+        st_emt.write("⏳正在思考,请稍等...")
+        knowledge = KnowledgeService(faiss_path, faiss_index)
+        response, cb = knowledge.query(chatgpt_model, user_question, candidate_number)
+        st_emt.write(response)
+        st.info(cb)
 
 
 if __name__ == '__main__':
