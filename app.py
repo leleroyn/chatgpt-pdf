9713d4dc912674a1ecdde368bd98a83073a2f7ee
Yong.Luo <leleroyn@qq.com>
2023-07-14 20:30:53 +0800

    优化

--- app.py
+++ app.py
@@ -24,26 +24,25 @@ def main():
         faiss_path = "db/sit"
 
     tab1, tab2 = st.tabs(["🏡回答问题", "🕝更新模型"])
-    tab2_emt = tab2.empty()
+
     # 上传文件
     pdf = tab2.file_uploader("上传PDF文件", type="pdf", help="不要频繁的更新知识库,不要上传大文件.", key="pdf")
 
     if tab2.button("更新模型↩️"):
-        with tab2_emt:
-            # 提取文本
-            if pdf is not None:
-                st.write("⏳正在更新模型...")
-                text = ""
-                with pdfplumber.open(pdf) as pdf_reader:
-                    for page in pdf_reader.pages:
-                        text += page.extract_text()
+        tab2_emt = tab2.empty()
+        # 提取文本
+        if pdf is not None:
+            tab2_emt.write("⏳正在更新模型...")
+            text = ""
+            with pdfplumber.open(pdf) as pdf_reader:
+                for page in pdf_reader.pages:
+                    text += page.extract_text()
 
-                knowledge = KnowledgeService(faiss_path, faiss_index)
-                knowledge.gen(text, os.getenv("SPLITTER_CHUCK_SIZE"), os.getenv("SPLITTER_CHUCK_OVER_LAP"))
-                st.success("✔️更新模型成功.")
-
-            else:
-                st.warning("请上传模型文件.")
+            knowledge = KnowledgeService(faiss_path, faiss_index)
+            knowledge.gen(text, os.getenv("SPLITTER_CHUCK_SIZE"), os.getenv("SPLITTER_CHUCK_OVER_LAP"))
+            tab2_emt.success("✔️更新模型成功.")
+        else:
+            tab2_emt.warning("请上传模型文件.")
 
     user_question = st.chat_input("❓来向我提问吧：")
     if user_question:
