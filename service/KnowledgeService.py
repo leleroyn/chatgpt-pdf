445cae865daba794ce6b23efaa9cc03e8ee12625
Yong.Luo <leleroyn@qq.com>
2023-07-14 18:03:15 +0800

    优化

--- service/KnowledgeService.py
+++ service/KnowledgeService.py
@@ -31,7 +31,8 @@ class KnowledgeService(object):
                                           index_name=self.index_name)
         llm = ChatOpenAI(model_name=model, temperature=0)
 
-        prompt_template = """Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer.
+        prompt_template = """Use the following pieces of context to answer the question at the end. If you don't know 
+        the answer, just say that you don't know, don't try to make up an answer.
 
         {context}
 
