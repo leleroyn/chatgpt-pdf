e1b8e25d95858104b905c95728498cea6fd4e113
Yong.Luo <leleroyn@qq.com>
2023-10-25 12:24:09 +0800

    ocr 识别时添加图像方向检测并旋转到正常位置

--- service/OcrService.py
+++ service/OcrService.py
@@ -1,9 +1,11 @@
 from io import BytesIO
+from math import fabs, sin, radians, cos
 
 import cv2
+import fitz
 import numpy as np
 from PIL import ImageOps, Image
-import fitz
+from rapid_orientation import RapidOrientation
 from rapidocr_onnxruntime import RapidOCR
 
 
@@ -23,9 +25,41 @@ def pil2cv(image):
     return new_image
 
 
+def rotate_bound(image, angle):
+    """
+     . 旋转图片
+     . @param image    opencv读取后的图像
+     . @param angle    (逆)旋转角度
+    """
+
+    h, w = image.shape[:2]  # 返回(高,宽,色彩通道数),此处取前两个值返回
+    newW = int(h * fabs(sin(radians(angle))) + w * fabs(cos(radians(angle))))
+    newH = int(w * fabs(sin(radians(angle))) + h * fabs(cos(radians(angle))))
+    M = cv2.getRotationMatrix2D((w / 2, h / 2), angle, 1)
+    M[0, 2] += (newW - w) / 2
+    M[1, 2] += (newH - h) / 2
+    return cv2.warpAffine(image, M, (newW, newH), borderValue=(255, 255, 255))
+
+
+def orientation(image_cv):
+    """
+    对含有文字信息的文档图像进行旋转
+    :param image_cv: cv 图像
+    :return: 处理完的正常方向图像
+    """
+    orientation_engine = RapidOrientation()
+    orientation_res, elapse = orientation_engine(image_cv)
+    print(orientation_res)
+    angle = int(orientation_res)
+    if angle > 0:
+        return rotate_bound(image_cv, angle)
+    else:
+        return image_cv
+
+
 class OcrService:
     def __init__(self):
-        self.rapid_ocr = RapidOCR()
+        self.rapid_ocr = RapidOCR(use_cls=False)
 
     def detect_from_image_path(self, image_path):
         image = Image.open(image_path)
@@ -33,12 +67,18 @@ class OcrService:
         return result
 
     def detect_from_image(self, image):
+        """
+        识别图像文本
+        :param image:  pil 图像
+        :return:
+        """
         image = image.convert('RGB')
-        image = rotate_image_by_exif(image)
+        # image = rotate_image_by_exif(image)
         image = pil2cv(image)
         img_w = 1024 if image.shape[1] > 1024 else image.shape[1]
         image = cv2.resize(image, (img_w, int(img_w * image.shape[0] / image.shape[1])),
                            interpolation=cv2.INTER_AREA)
+        image = orientation(image)
         B_channel, G_channel, R_channel = cv2.split(image)
         res, elapse = self.rapid_ocr(R_channel)
         result = []
